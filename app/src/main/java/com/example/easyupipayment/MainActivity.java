f0719691059c0ba564a8b581a73c62aebeb4185d
Aniruddha Deshmukh <anideshmukh004@gmail.com>
2019-11-14 23:27:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/easyupipayment/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.easyupipayment;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.shreyaspatil.EasyUpiPayment.EasyUpiPayment;
+import com.shreyaspatil.EasyUpiPayment.listener.PaymentStatusListener;
+import com.shreyaspatil.EasyUpiPayment.model.TransactionDetails;
+
+public class MainActivity extends AppCompatActivity implements PaymentStatusListener {
+
+    private ImageView imageView;
+    private TextView statusView;
+    private Button payButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Initialize Components
+        imageView = findViewById(R.id.imageView);
+        statusView = findViewById(R.id.textView_status);
+        payButton = findViewById(R.id.button_pay);
+
+        //Create instance of EasyUpiPayment
+        final EasyUpiPayment easyUpiPayment = new EasyUpiPayment.Builder()
+                .with(this)
+                .setPayeeVpa("example@vpa")
+                .setPayeeName("PAYEE_NAME")
+                .setTransactionId("TRANSACTION_ID")
+                .setTransactionRefId("TRANSACTION_REF_ID")
+                .setDescription("DESCRIPTION_OR_SHORT_NOTE")
+                .setAmount("AMOUNT IN XX.XX DECIMAL FORMAT")
+                .build();
+
+        //Register Listener for Events
+        easyUpiPayment.setPaymentStatusListener(this);
+
+        //Proceed for Payment on click
+        payButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                easyUpiPayment.startPayment();
+            }
+        });
+    }
+
+    @Override
+    public void onTransactionCompleted(TransactionDetails transactionDetails) {
+        // Transaction Completed
+        Log.d("TransactionDetails", transactionDetails.toString());
+        statusView.setText(transactionDetails.toString());
+    }
+
+    @Override
+    public void onTransactionSuccess() {
+        // Payment Success
+        Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show();
+        imageView.setImageResource(R.drawable.ic_success);
+    }
+
+    @Override
+    public void onTransactionSubmitted() {
+        // Payment Pending
+        Toast.makeText(this, "Pending | Submitted", Toast.LENGTH_SHORT).show();
+        imageView.setImageResource(R.drawable.ic_success);
+    }
+
+    @Override
+    public void onTransactionFailed() {
+        // Payment Failed
+        Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show();
+        imageView.setImageResource(R.drawable.ic_failed);
+    }
+
+    @Override
+    public void onTransactionCancelled() {
+        // Payment Cancelled by User
+        Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show();
+        imageView.setImageResource(R.drawable.ic_failed);
+    }
+}
